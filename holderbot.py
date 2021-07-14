2a51c474f1d5ed305c64e5ea1b92f11aae6354dd
0xOnga <86559810+0xOnga@users.noreply.github.com>
2021-07-14 13:51:53 +0200

    Update holderbot.py

--- holderbot.py
+++ holderbot.py
@@ -14,6 +14,8 @@ async def on_ready():
     print('bot is ready')
     holdersloop.start()
 
+    
+    #bot name loop
 @tasks.loop(seconds = 3600)
 async def holdersloop():
     mint = "" #token mint
@@ -45,6 +47,39 @@ async def holdersloop():
     pricebot = guild.get_member()  #put your bot id in the brackets
     await pricebot.edit(nick=f'{count:,} HOLDERS')
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Holders made by @_Onga_" ))
+    
+    
+    
+    
+    #command to retrieve holders
+@client.command()
+async def holders(ctx):
+    mint = "" #token mint
+    endpoint = "https://api.mainnet-beta.solana.com"
+    headers = {
+            'Content-Type': 'application/json',
+        }   
+    data = ' { "jsonrpc": "2.0", "id": 1, "method": "getProgramAccounts", "params": [ "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA", { "encoding": "jsonParsed", "filters": [ { "dataSize": 165 }, { "memcmp": { "offset": 0, "bytes": "' + f"{mint}" + '" } } ] } ] } '
+    response = requests.post(endpoint, headers=headers, data=data)
+
+    r = response.json()
+
+    
+    rstr = json.dumps(r)
+
+    data = json.loads(rstr)
+
+    n = rstr.count('pubkey')
+
+    #clear out 0 balance holders
+    count = 0
+    for i in range(n):
+        if (data['result'][i]['account']['data']['parsed']['info']['tokenAmount']['uiAmount']) != 0:
+            count= count + 1
+
+            
+    
+    await ctx.send('TOKEN_NAME currently has ' + f'{count:,}' + ' holders')
 
 
 client.run('') #your bot token
