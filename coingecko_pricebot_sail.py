520378e8ce1674e3bd06e63110e3c00a8a4a76ac
0xOnga <86559810+0xOnga@users.noreply.github.com>
2021-09-05 22:36:17 +0200

    Update coingecko_pricebot_sail.py

--- coingecko_pricebot_sail.py
+++ coingecko_pricebot_sail.py
@@ -33,6 +33,23 @@ async def priceloop():
     await pricebot.edit(nick="SAIL %.3f$" % price['sail']['usd'] )
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="SAIL Price bot made by @_Onga_"))
     
+@client.command()
+async def drop(ctx):
+    
+    rSail = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=sail&vs_currencies=usd")
+   
+    rgSail = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=solanasail-governance-token&vs_currencies=usd")
+        
+    rSail = rSail.json()
+    
+    rgSail = rgSail.json()
+    
+    SailValue = 21000 * rSail['sail']['usd']
+
+    gSailValue = 1500 * rgSail['solanasail-governance-token']['usd']
+    
+    await ctx.channel.send('Airdrop value is ' + str(SailValue+gSailValue) + '$ (SAIL : ' + str(SailValue) + '$ + gSail : ' + str(gSailValue) +'$)')
+
 
 
 
